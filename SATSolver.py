e600dae5f9c2b4d100b36a59ac33fcd74179d57f
rcgo234 <160293661+rcgo234@users.noreply.github.com>
2024-10-18 10:12:00 -0400

    Update SATSolver.py
    Base form of the code.

--- SATSolver.py
+++ SATSolver.py
@@ -1,1 +1,16 @@
+class Slovers:
+  def __init__(self):
+    self.numVars = 0
+    self.numClauses = 0
+    self.clauses = []
 
+  def DPLL(self):
+    return
+
+  def WalkSAT(self):
+    return
+
+  def LoacalSearch(self):
+    return
+
+# Define any helper functions
